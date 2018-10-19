3d400149e11b9232441e04542d2119ffd65e440e
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2018-10-19 11:18:17 -0500

    reverse parentheses ok

--- src/ReverseParentheses.java
+++ src/ReverseParentheses.java
@@ -1,13 +1,34 @@
 public class ReverseParentheses {
   public static void main(String args[]){
     String s = "a(bcdefghijkl(mno)p)q";
-    String s2 = "co(de(fight)s)";
+    String s2 = "The ((quick (brown) (fox) jumps over the lazy) dog)";
     PlaygroundJava.printLine(reverseParentheses(s2));
   }
 
   static String reverseParentheses(String s){
-    String[] split = s.split("[\\(||\\)]");
+    int start = s.lastIndexOf("(");
+    int end = 0;
+    String stringInParentheses ;
+    StringBuilder reverse = new StringBuilder();
+    if(start !=-1){
+      for(int c = start; c <s.length(); ++c) {
+        if(String.valueOf(s.charAt(c)).equals(")")){
+          end = c;
+          break;
+        }
+      }
+    }
 
-    return s;
+    if (start != -1 && end != 0){
+        stringInParentheses = s.substring(start+1,end);
+        for (int i = stringInParentheses.length() -1 ;i >= 0; --i) {
+          reverse.append(String.valueOf(stringInParentheses.charAt(i)));
+        }
+        s = s.replace("("+stringInParentheses+")",reverse);
+        return reverseParentheses(s);
+      }
+      else{
+        return s;
+      }
   }
 }
