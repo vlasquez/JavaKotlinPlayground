6476b7c8838d7e26fcf5fa6bf094ced4f657af47
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2018-10-24 17:14:47 -0500

    HalvingSum solved Ok

--- src/halvingSum.java
+++ src/halvingSum.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class halvingSum {
+
+  public static void main(String args[]){
+    int a = 4, b = 13, n = 25;
+    PlaygroundJava.printLine(halvingSum(n));
+  }
+
+  static int halvingSum(int n) {
+    int res = 0;
+    int i = 1;
+    while(n>=1){
+      res+=n;
+      n = Math.round(n/2);
+    }
+    return res;
+  }
 }
